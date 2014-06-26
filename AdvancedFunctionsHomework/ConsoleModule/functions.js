2b5cd7937790c96f1dd9f0572de6eb0f69138724
d-kostov-dev <d.kostov.dev@gmail.com>
2014-06-26 12:28:11 +0300

    Console Module

---
+++ AdvancedFunctionsHomework/ConsoleModule/functions.js
@@ -0,0 +1,36 @@
+﻿var specialConsole = (function specialConsole() {
+    function processInput() {
+        var regExp,
+            inputText = arguments[0];
+
+        if (arguments.length > 1) {
+            for (var i = 0, len = arguments.length; i < len; i++) {
+                regExp = new RegExp("{(" + i + ")}", 'g');
+                inputText = inputText.replace(regExp, arguments[i + 1]);
+            }
+        }
+
+        return inputText.toString();
+    }
+
+    function writeLine() {
+        var input = processInput.apply(null, arguments);
+        console.log(input);
+    }
+
+    function writeError() {
+        var input = processInput.apply(null, arguments);
+        console.error(input);
+    }
+
+    function writeWarning() {
+        var input = processInput.apply(null, arguments);
+        console.warn(input);
+    }
+
+    return {
+        writeLine: writeLine,
+        writeError: writeError,
+        writeWarning: writeWarning
+    };
+}());
