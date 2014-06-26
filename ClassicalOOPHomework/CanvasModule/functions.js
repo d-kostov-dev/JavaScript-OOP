734d97d59f983fd36db6f5e99a360450d135e16f
d-kostov-dev <d.kostov.dev@gmail.com>
2014-06-26 15:07:00 +0300

    CanvasModule

---
+++ ClassicalOOPHomework/CanvasModule/functions.js
@@ -0,0 +1,74 @@
+﻿var CanvasModule = function () {
+    // Creating a canvas object and inits canvas options
+    function Canvas(selector, width, height) {
+        self = this;
+        self.canvasNode = document.querySelector(selector);
+        self.ctx = self.canvasNode.getContext("2d");
+
+        self.canvasNode.width = width;
+        self.canvasNode.height = height;
+
+        // PRIVATE FUNCS
+        // Used for each shape
+        function startDrawingCommands() {
+            self.ctx.beginPath();
+            self.ctx.save();
+        }
+
+        function endDrawingCommands(fillColor, strokeColor) {
+            if (fillColor) {
+                self.ctx.fillStyle = fillColor;
+            } else {
+                // Default Fill Color
+                self.ctx.fillStyle = "#000";
+            }
+
+            // If no stroke color...the stroke is skipped
+            if (strokeColor) {
+                self.ctx.strokeStyle = strokeColor;
+                self.ctx.stroke();
+            }
+
+            self.ctx.fill();
+            self.ctx.restore();
+        }
+
+        // Get radian from degrees input
+        function getRadian(degrees) {
+            return degrees * Math.PI / 180;
+        }
+
+        // PUBLIC FUNCS
+        // Add Rectangle Function
+        function addRect(posX, posY, width, height, fillColor, strokeColor) {
+            startDrawingCommands()
+            self.ctx.rect(posX, posY, width, height);
+            endDrawingCommands(fillColor, strokeColor);
+        }
+
+        // Add Circle Function
+        function addCircle(posX, posY, radius, fillColor, strokeColor) {
+            startDrawingCommands()
+            self.ctx.arc(posX, posY, radius, 0, getRadian(360)); // Full Circle
+            endDrawingCommands(fillColor, strokeColor);
+        }
+
+        // Add Line Function
+        function addLine(startX, startY, endX, endY, fillColor, strokeColor) {
+            startDrawingCommands()
+            self.ctx.moveTo(startX, startY);
+            self.ctx.lineTo(endX, endY);
+            endDrawingCommands(fillColor, strokeColor);
+        }
+
+        return {
+            addRect: addRect,
+            addCircle: addCircle,
+            addLine: addLine
+        }
+    }
+
+    return {
+        Canvas: Canvas
+    };
+}();
---
+++ ClassicalOOPHomework/CanvasModuleV2/functions.js
@@ -0,0 +1,79 @@
+﻿// Using Prototype
+function Canvas(selector, width, height) {
+    self = this;
+    self.canvasNode = document.querySelector(selector);
+    self.ctx = self.canvasNode.getContext("2d");
+    self.canvasNode.width = width;
+    self.canvasNode.height = height;
+}
+
+Canvas.prototype.addRect = function (posX, posY, width, height, fillColor, strokeColor) {
+    this.ctx.beginPath();
+    this.ctx.save();
+    this.ctx.rect(posX, posY, width, height);
+
+    if (fillColor) {
+        this.ctx.fillStyle = fillColor;
+    } else {
+        // Default Fill Color
+        this.ctx.fillStyle = "#000";
+    }
+
+    // If no stroke color...the stroke is skipped
+    if (strokeColor) {
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.stroke();
+    }
+
+    this.ctx.fill();
+    this.ctx.restore();
+}
+
+Canvas.prototype.addCircle = function (posX, posY, radius, fillColor, strokeColor) {
+    this.ctx.beginPath();
+    this.ctx.save();
+    this.ctx.arc(posX, posY, radius, 0, this.getRadian(360)); // Full Circle
+
+    if (fillColor) {
+        this.ctx.fillStyle = fillColor;
+    } else {
+        // Default Fill Color
+        this.ctx.fillStyle = "#000";
+    }
+
+    // If no stroke color...the stroke is skipped
+    if (strokeColor) {
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.stroke();
+    }
+
+    this.ctx.fill();
+    this.ctx.restore();
+}
+
+Canvas.prototype.addLine = function (startX, startY, endX, endY, fillColor, strokeColor) {
+    this.ctx.beginPath();
+    this.ctx.save();
+    this.ctx.moveTo(startX, startY);
+    this.ctx.lineTo(endX, endY);
+
+    if (fillColor) {
+        this.ctx.fillStyle = fillColor;
+    } else {
+        // Default Fill Color
+        this.ctx.fillStyle = "#000";
+    }
+
+    // If no stroke color...the stroke is skipped
+    if (strokeColor) {
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.stroke();
+    }
+
+    this.ctx.fill();
+    this.ctx.restore();
+}
+
+Canvas.prototype.getRadian = function getRadian(degrees) {
+    return degrees * Math.PI / 180;
+}
